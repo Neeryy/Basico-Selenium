25cf1356f8fe63fdf58cb75afce41be037ffab84
Gabriel Nery <gabrielnery894@gmail.com>
2024-08-26 23:10:19 -0300

    selenium

---
+++ aula01.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+servico = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=servico)
+
+navegador.get(r'\formulario.html')
+
+navegador.find_element(By.XPATH, '//*[@id="header"]/div/div/div[1]/a/img').click()
+
+
+
+
+
